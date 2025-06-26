159ebcb15382626d6acda788619e7920ce055909
Mitz Bitz <mitchellvieira12@gmail.com>
2025-06-26 17:04:06 -0400

    accessibility changes + title change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import isspic from '../assets/Isabelle.webp'
+import tomPic from '../assets/tom.gif'
 
 export default function Header() {
 
 
   return (
     <div className="headDiv">
-      <h1 className="header">ACQuizzes <img src={isspic}  width={75}/></h1>
+      <div className="newDiv">
+      <h1 className="header">Cr
+      <img src={tomPic} width={60} />
+      ssing Quizzes</h1>
+      </div>
       <div className="linkDiv">
         <Link to="/">Home</Link> {" "}  
         <Link to="/about">About</Link> {" "}      
